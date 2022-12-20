aea9597631bd673f4d1feff0b390aefd9aaf3938
DohyaOriginalNickname <tsareg.n@gmail.com>
2022-12-20 12:57:38 +0600

    create the modal window at settings page

--- src/components/settings/settingsPage/settingsPage.js
+++ src/components/settings/settingsPage/settingsPage.js
@@ -18,91 +18,134 @@ import settingsIcon from '../../../assets/navigation/activeIcons/ActiveSettingsI
 const SettingsPage = () => {
 
     const [userData, setUserData] = useState({})
+    const [modal, setModal] = useState(false)
+
+    const cancel = () => {
+        setModal(modal => !modal)
+    }
 
     useEffect(() => {
         setUserData(JSON.parse(localStorage.user))
     }, [])
 
+
     return (
-        <div className="settings-page">
+        <>
+            <div className={modal ? 'modal-bg' : null}></div>
+            <div className={modal ? "settings-page no-touch" : "settings-page"}>
 
-            <div className='aaaa'>
-                <img src={arrow} alt="arrow" />
-                <p>Настройки</p>
-            </div>
-
-            <div className='all-settings'>
-                <div className='main-settings'>
-                    <div className='profile'>
-                        <img src={avatar} alt="" />
-                        <div className='profile__info'>
-                            <p>{userData.surname} {userData.name} {userData.middleName}</p>
-                            <p>{userData.email}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <Link to={'/changeUserData'}>
-                            <button className='edit-button'>Редактировать</button>
-                        </Link>
-                    </div>
-                </div>
-
-                <div className='others-settings'>
-                    <div className="border"></div>
-
-                    <div className='change-theme'>
-                        <p>Внешний вид</p>
-                        <div className='select-theme'>
-                            <img src={palitra} alt="" />
-                            <div className='info-of-theme'>
-                                <p>Темная тема</p>
-                                <p>Включена</p>
-                            </div>
-                            <img src={arrow2} alt="" />
-                        </div>
-                    </div>
-
-                    <div className="border"></div>
-
-                    <div className='about'>
-                        <p>О приложении</p>
-                        <p>Зигерионцы помещают Джерри и Рика в симуляцию, чтобы узнать секрет изготовления концен-трирован- ной темной материи.</p>
-                    </div>
-
-                    <div className="border"></div>
-
-                    <div className='version'>
-                        <p>Версия приложения</p>
-                        <p>Rick & Morty  v1.0.0</p>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div className="navigation-panel">
-                <Link to={'/listOfCharacters'}>
-                    <div className="navigation-panel__item">
-                        <img src={characterIcon} alt="" />
-                        <p>Персонажи</p>
-                    </div>
-                </Link>
-                <Link to={'/listOfLocaions'}>
-                    <div className="navigation-panel__item">
-                        <img src={locatioIcon} alt="" />
-                        <p>Локации</p>
-                    </div>
-                </Link>
-                <Link to={'/listOfEpisods'}>
-                    <div className="navigation-panel__item">
-                        <img src={episodeIcon} alt="" />
-                        <p>Эпизоды</p>
-                    </div>
-                </Link>
-                <div className="navigation-panel__item_select">
-                    <img src={settingsIcon} alt="" />
+                <div className='aaaa'>
+                    <img src={arrow} alt="arrow" />
                     <p>Настройки</p>
                 </div>
+
+                <div className='all-settings'>
+                    <div className='main-settings'>
+                        <div className='profile'>
+                            <img src={avatar} alt="" />
+                            <div className='profile__info'>
+                                <p>{userData.surname} {userData.name} {userData.middleName}</p>
+                                <p>{userData.email}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <Link to={'/changeUserData'}>
+                                <button className='edit-button'>Редактировать</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className='others-settings'>
+                        <div className="border"></div>
+
+                        <div className='change-theme'>
+                            <p>Внешний вид</p>
+                            <div className='select-theme' onClick={() => setModal(modal => !modal)}>
+                                <img src={palitra} alt="" />
+                                <div className='info-of-theme'>
+                                    <p>Темная тема</p>
+                                    <p>Включена</p>
+                                </div>
+                                <img src={arrow2} alt="" />
+                            </div>
+                        </div>
+
+                        <div className="border"></div>
+
+                        <div className='about'>
+                            <p>О приложении</p>
+                            <p>Зигерионцы помещают Джерри и Рика в симуляцию, чтобы узнать секрет изготовления концен-трирован- ной темной материи.</p>
+                        </div>
+
+                        <div className="border"></div>
+
+                        <div className='version'>
+                            <p>Версия приложения</p>
+                            <p>Rick & Morty  v1.0.0</p>
+                        </div>
+                    </div>
+                </div>
+
+                {modal ? <ChangeThemeModal cancel={cancel} /> : null}
+
+                <div className="navigation-panel">
+                    <Link to={'/listOfCharacters'}>
+                        <div className="navigation-panel__item">
+                            <img src={characterIcon} alt="" />
+                            <p>Персонажи</p>
+                        </div>
+                    </Link>
+                    <Link to={'/listOfLocaions'}>
+                        <div className="navigation-panel__item">
+                            <img src={locatioIcon} alt="" />
+                            <p>Локации</p>
+                        </div>
+                    </Link>
+                    <Link to={'/listOfEpisods'}>
+                        <div className="navigation-panel__item">
+                            <img src={episodeIcon} alt="" />
+                            <p>Эпизоды</p>
+                        </div>
+                    </Link>
+                    <div className="navigation-panel__item_select">
+                        <img src={settingsIcon} alt="" />
+                        <p>Настройки</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+const ChangeThemeModal = (props) => {
+    return (
+        <div className='change-theme-modal'>
+            <div className='title'>
+                <p>Темная тема</p>
+            </div>
+
+            <ul className='select-list'>
+                <li>
+                    <input type="radio" name='theme' value="off" id='off' />
+                    <label htmlFor='off'>Выключенна</label>
+                </li>
+                <li>
+                    <input type="radio" name='theme' value="on" id='on' />
+                    <label htmlFor='on'>Включенна</label>
+                </li>
+                <li>
+                    <input type="radio" name='theme' value="for-the-system" id='for-the-system' />
+                    <label htmlFor='for-the-system'>Следовать настройкам системы</label>
+                </li>
+                <li>
+                    <input type="radio" name='theme' value="low-energy" id='low-energy' />
+                    <label htmlFor='low-energy'>В режиме энергосбережения</label>
+                </li>
+            </ul>
+
+            <div className='cancel'>
+                <p onClick={() => props.cancel()}>Отмена</p>
             </div>
         </div>
     )
